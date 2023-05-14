67791a3f5ecce32042efb6cc6c4c0fe3a29db0f3
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2023-05-14 15:56:38 +0700

    Clean Code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import qrcode
 import requests
-from concurrent.futures import ThreadPoolExecutor
 import pathlib
+from concurrent.futures import ThreadPoolExecutor
 
 # Cache for frequently accessed URLs
 url_cache = {}
@@ -12,8 +12,7 @@ def shorten_url(url):
         return url_cache[url]
 
     try:
-        response = requests.get("http://is.gd/create.php",
-                                params={"format": "simple", "url": url})
+        response = requests.get("http://is.gd/create.php", params={"format": "simple", "url": url})
         if response.status_code == 200:
             short_url = response.text
             url_cache[url] = short_url
@@ -24,24 +23,14 @@ def shorten_url(url):
     return None
 
 
-def read_urls_from_file(file_path):
+def read_file(file_path):
     with open(file_path, "r") as f:
-        return [url.strip() for url in f]
+        return [line.strip() for line in f]
 
 
-def write_shortened_urls_to_file(shortened_urls, file_path):
+def write_file(lines, file_path):
     with open(file_path, "w") as f:
-        f.write("\n".join(shortened_urls))
-
-
-def print_shortened_urls(shortened_urls):
-    print("Shortened URLs:")
-    print("\n".join(shortened_urls))
-
-
-def read_text_file(filename):
-    with open(filename, "r") as f:
-        return f.read()
+        f.write("\n".join(lines))
 
 
 def generate_qr_code(content, index, foldername):
@@ -49,22 +38,12 @@ def generate_qr_code(content, index, foldername):
     qr.add_data(content)
     qr.make(fit=True)
     filename = f"QR_{index}.png"
-    save_qr_code(qr, filename, foldername)
-
-
-def save_qr_code(qr, filename, foldername):
     path = pathlib.Path(foldername)
     path.mkdir(parents=True, exist_ok=True)
     filepath = path.joinpath(filename)
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(filepath)
     print(f"{filepath} generated successfully!")
-
-
-def generate_qr_codes(shortened_urls, foldername="QR_codes"):
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        for i, url in enumerate(shortened_urls):
-            executor.submit(generate_qr_code, url, i+1, foldername)
 
 
 def process_url(url):
@@ -76,7 +55,8 @@ def process_url(url):
 def main():
     input_file_path = "input.txt"
     shortened_output_file_path = "shortened_urls.txt"
-    urls = read_urls_from_file(input_file_path)
+    qr_foldername = "QR_codes"
+    urls = read_file(input_file_path)
 
     # Shorten URLs
     with ThreadPoolExecutor(max_workers=50) as executor:
@@ -84,12 +64,15 @@ def main():
     shortened_urls = [url for url in shortened_urls if url is not None]
 
     # Write shortened URLs to file
-    write_shortened_urls_to_file(shortened_urls, shortened_output_file_path)
+    write_file(shortened_urls, shortened_output_file_path)
     print(f"Shortened URLs written to {shortened_output_file_path}")
-    print_shortened_urls(shortened_urls)
+    print("Shortened URLs:")
+    print("\n".join(shortened_urls))
 
     # Generate QR codes
-    generate_qr_codes(shortened_urls)
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        for i, url in enumerate(shortened_urls):
+            executor.submit(generate_qr_code, url, i+1, qr_foldername)
 
 
 if __name__ == "__main__":
